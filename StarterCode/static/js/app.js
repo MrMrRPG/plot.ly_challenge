a28c563d5f9e3a832c172489d51ad746f7550071
MrMrRPG <nng5998@gmail.com>
2021-09-17 11:34:25 -0700

    Not sure if I did everything right but I did something

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,6 +1,28 @@
 // 1. Use the D3 library to read samples.json
 const samples = "./samples.json";
 
-d3.json(samples).then(function(data){
-	console.log(data)
+d3.json(samples).then(function(data1){
+	console.log(data1)
 });
+
+// 2. Create a horizontal bar chart with a dropdown menu to display the top 10
+// OTUs found in that individual.
+
+// Use *sample_values* as the values for the bar chart.
+// Use *otu_ids* as the labels for the bar chart.
+// USE *otu_labels* as the hovertext for the chart.
+// function values(samples){
+// 	return samples.sample_values;
+// }
+// let sample_values = data.filter(values);
+
+var sv = Object.values(data1.sample_values)
+
+// let trace1 ={
+// 	x: ,
+// 	y: ,
+// 	type: 'bar'
+// }
+// let data = [trace1]
+
+// Plotly.newPlot("plot", data, layout)
