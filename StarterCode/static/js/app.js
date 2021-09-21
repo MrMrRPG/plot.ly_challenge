0c6c777a0aea5dcecab444711bdca1ff5466d159
MrMrRPG <nng5998@gmail.com>
2021-09-20 22:01:51 -0700

    got plot and bubble working

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -9,22 +9,62 @@ let data = d3.json(sample).then(function(data){
 // Use *sample_values* as the values for the bar chart.
 // Use *otu_ids* as the labels for the bar chart.
 // USE *otu_labels* as the hovertext for the chart.
-var sample_values = Object.values(data.sample_values);
-var otu_ids = Object.values(data.otu_ids);
-var otu_labels = Object.values(data.otu_labels);
+
+var tempt = data.samples[0];
+console.log(tempt)
+
+let sampleValues = tempt.sample_values
+let otuIDs = tempt.otu_ids
+let otuLabels = tempt.otu_labels
+
+slicedData = sampleValues.slice(0, 10);
+slicedIDs = otuIDs.slice(0, 10);
+slicedLabels = otuLabels.slice(0, 10);
+// slicedData = sampleValues.slice(0, 10);
+
+slicedIDs = slicedIDs.map(L => 'otu' + L)
+slicedData.reverse();
+
+let trace1 = {
+	x: slicedData,
+	y: slicedIDs,
+	type: 'bar',
+	orientation: 'h',
+	text: slicedLabels
+};
+
+let bar1 = [trace1];
+
+let layout = {
+	title: "OTU Status" 
+};
+
+Plotly.newPlot("bar", bar1, layout);
+
+// 3. Create a bubble char that displays each sample.
+let trace2 = {
+	y: sampleValues,
+	x: otuIDs,
+	mode: 'markers',
+	marker: {
+		size: sampleValues,
+		color: otuIDs,
+	},
+	text: slicedLabels,
+};
+
+let bubble = [trace2];
+
+let layout2 = {
+	title: "Individual OTU" 
+};
+
+Plotly.newPlot("bubble", bubble, layout2);
+
+
 
 });
 
 
+
 console.log(data)
-
-// var sv = Object.values(sample.sample_values)
-
-// let trace1 ={
-// 	x: ,
-// 	y: ,
-// 	type: 'bar'
-// }
-// let data = [trace1]
-
-// Plotly.newPlot("plot", data, layout)
